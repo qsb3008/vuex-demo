e24fcdcce1622362643890441345cb2a3e620709
qsb3008 <qsb3008@163.com>
2019-09-28 16:53:53 +0800

    demo commit

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div style="padding: 20px;" @click="increment">
+      {{count}}
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  computed: {
+    count () {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      this.$store.commit('increment')
+      console.log(this.$store.state.count)
+    }
+  }
+}
+</script>
